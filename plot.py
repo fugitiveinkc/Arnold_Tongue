2788723ea5106b5564456ee6ee353b52c6808cea
fugitiveinkc <blacksg1909@gmail.com>
2015-11-25 00:50:28 -0600

    First commit

---
+++ plot.py
@@ -0,0 +1,16 @@
+'''
+
+Project: Plotting circle maps where arnold tongues are present
+
+Notes:
+	-"Arnold tongue of a finite-parameter family of circle maps, is a region in the space of parameters where the map has locally-constant rational rotation number."
+		-Things to dissect:
+			1) finite-parameter of circle maps
+			2) locally-constant rational rotation number
+	-Useful resources:
+		1) mathematica.stackexchange.com -- the winding number for the circle map (Arnold tongue)
+		2) Wikipedia -- Arnold Tongue
+
+
+'''
+
